e349146889e8ad636814df1860197985d59bb296
Carlos <almendracarloshenrique@gmail.com>
2025-03-14 23:03:16 -0300

    add paymentProcessor

--- src/processors/PaymentProcessor.java
+++ src/processors/PaymentProcessor.java
@@ -1,5 +1,6 @@
 package processors;
 
+import factory.PaymentFactory;
 import interfaces.PaymentStrategy;
 import paymentMethods.BoletoPayment;
 import paymentMethods.CreditCardPayment;
@@ -9,22 +10,10 @@ import java.util.Map;
 
 public class PaymentProcessor {
 
-    private final Map<Integer, PaymentStrategy> mapStrategy = Map.of(
-            1, new PixPayment(),
-            2, new CreditCardPayment(),
-            3, new BoletoPayment()
-    );
-
     public PaymentProcessor() {}
 
-    public void selectPaymentStrategy(Integer option, Double value) {
-        PaymentStrategy strategy = mapStrategy.get(option);
-
-        if (strategy == null) {
-            System.out.println("\nInvalid option. Please select a valid payment method.");
-            return;
-        }
-
+    public void processPayment(int option, double value) {
+        PaymentStrategy strategy = PaymentFactory.createPaymentMethod(option);
         strategy.processPayment(value);
     }
 
