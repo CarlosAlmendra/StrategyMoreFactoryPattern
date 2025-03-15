f1c26b9e2acea988d7d6ee1802f6729b5c35b764
Carlos <almendracarloshenrique@gmail.com>
2025-03-14 23:03:36 -0300

    modificações main

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         PaymentProcessor paymentProcessor = new PaymentProcessor();
 
-        System.out.print("Select payment method:\n1- PIX\n2- CARTAO DE CRÉDITO\n3- BOLETO\nR:");
-        Integer option = sc.nextInt();
-        System.out.print("\nEnter the transaction amount:: \nR:");
-        Double value = sc.nextDouble();
+        System.out.print("Select payment method:\n1- PIX\n2- CREDIT CARD\n3- BOLETO\nR: ");
+        int option = sc.nextInt();
 
-        paymentProcessor.selectPaymentStrategy(option, value);
+        System.out.print("\nEnter the transaction amount: R$ ");
+        double value = sc.nextDouble();
+
+        paymentProcessor.processPayment(option, value);
         sc.close();
     }
 }
